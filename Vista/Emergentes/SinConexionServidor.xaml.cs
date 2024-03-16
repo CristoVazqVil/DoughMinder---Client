b766e51174b6a6055f8864a721ae7188616daf5d
CrisVazV <cris990077@hotmail.com>
2024-03-16 16:44:58 -0600

    Ventanas de CU y emergentes

---
+++ Vista/Emergentes/SinConexionServidor.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DoughMinder___Client.Vista.Emergentes
+{
+    /// <summary>
+    /// Interaction logic for SinConexionServidor.xaml
+    /// </summary>
+    public partial class SinConexionServidor : Window
+    {
+        public SinConexionServidor()
+        {
+            InitializeComponent();
+        }
+    }
+}
